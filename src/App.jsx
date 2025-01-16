580ae7ac0b42e0b7bb7094894d8717c24f08a00e
ArabDeveloper <arabdevelopers731@gmail.com>
2025-01-16 09:48:42 +0200

    1.functional component -write functional component -use states and props to create fully dynamic component

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+// props and states
+import React from 'react'
+import Counter from './components/Counter'
+
+const App = () => {
+  return (
+    <div>
+
+      <Counter
+      title = "counter 1 "
+      
+      />
+
+      <Counter
+            title = "counter 2 "
+
+      />
+
+      <Counter
+            title = "counter 3 "
+
+      />
+
+
+    </div>
+  )
+}
+
+export default App
