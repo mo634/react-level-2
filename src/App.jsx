6604f7e421919767edea62b9442eda1447dc7f2d
ArabDeveloper <arabdevelopers731@gmail.com>
2025-01-17 07:42:40 +0200

    conditional rendering if else(statement)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 // conditional rendering 
 //1. ternary operator (expression) allowed in JSX ✅
+// -to write if statement in JSX
+
 //2. logical AND operator(expression) allowed in JSX 
+// -to write if statement in JSX without else value 
+
+
 //3. if else(statement)not allowed in JSX
 
 
@@ -9,35 +14,22 @@ import React from 'react'
 const App = () => {
   const isLoggedIn = true
   const isHasLinkedin = false
+  //out jsx  
+  if (isLoggedIn && isHasLinkedin) {
+    return (
+      <div>
+        <h1>Welcome to my app</h1>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div>
+        <h1>Please sign up</h1>
+      </div>
+    )
+  }
 
-  return (
-    <div>
-      {/* if isLogged in is true show user logged in
-      else show user not logged in 
-      */}
-
-
-      <h1>
-        {
-          // ternary operator 
-          isLoggedIn ? "user logged in " : "not logged in "
-        }
-      </h1>
-
-      <h1>
-        {/* {
-          isHasLinkedin ? "user has linkedin"
-            :
-            null
-        } */}
-
-        {
-          // logical AND operator
-          isHasLinkedin && "user has linkedin"
-        }
-      </h1>
-    </div>
-  )
 }
 
 export default App
