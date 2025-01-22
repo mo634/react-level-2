9a93c47d02c62c805b3166c4dae7a5c026c80fc2
ArabDeveloper <arabdevelopers731@gmail.com>
2025-01-22 14:31:19 +0200

    9.Lists and keys part 1 use map

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,18 @@
-// 1️⃣ practical example and use case for  conditional rendering
-import './style.css'
-import React, { useState } from 'react'
-import { FaRegCopy } from "react-icons/fa";
-import { FaCheck } from "react-icons/fa";
+// Section 2
+//1️⃣ Lists And Keys
+import React from 'react'
+import People from './components/People'
 
 const App = () => {
-
-  const [isClicked, setIsClicked] = useState(false)
-  const [content, setContent] = useState("mohamed")
-  // write logic
-
-  const handleCopy = async () => {
-    setIsClicked(true)
-
-    await navigator.clipboard.writeText(content)
-
-    setTimeout(() => {
-      setIsClicked(false)
-    }, 3000);
+  const myArr = ["home", "about", "info"]
 
 
 
-  }
   return (
-    <div className='container'>
-
-      <p className='content'>{content}</p>
-
-      <button onClick={handleCopy} className='copy-button' >
-        {
-          isClicked ? <FaCheck /> : <FaRegCopy />
-        }
-      </button>
+    <div>
+      {
+        myArr.map((currentElement) => <People page={currentElement} />)
+      }
 
 
     </div>
---
+++ src/components/People.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const People = ({ page }) => {
+    return (
+        <div className="">
+            <h1>{page} page</h1>
+            <ul>
+                <li>Mohamed CSS </li>
+                <li>Ahmed IT </li>
+                <li>Ali AI </li>
+                <li>Mahmoud Cyber Security </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+                <li>Osama Web </li>
+            </ul>
+            <hr />
+        </div>
+    )
+}
+
+export default People
