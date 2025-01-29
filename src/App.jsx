7ab2602bcb6de5c911d4f67a4bbe4ff4e7418649
ArabDeveloper <arabdevelopers731@gmail.com>
2025-01-29 10:12:19 +0200

    12.Lists And Keys add the unique key

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,76 @@
-// Section 2
-//1️⃣ Lists And Keys
-import React from 'react'
-import People from './components/People'
+// Keys=> index by default 
+//1. reorder using keys is more efficient 
+
+import React, { useState } from 'react'
 
 const App = () => {
-  const myArr = ["home", "about", "info"]
+  const [data, setData] = useState([
+    {
+      id: 1,
+      name: "a"
+    }
+    ,
+    {
+      id: 2,
+      name: "b"
+    }
+    ,
+    {
+      id: 3,
+      name: "c"
+    }
+  ])
 
+  const handleShuffle = () => {
+    setData([
+      data[1],
+      data[2],
+      data[0]
+    ])
+  }
 
+  const handleReset = () => {
+    setData([
+      {
+        id: 1,
+        name: "a"
+      }
+      ,
+      {
+        id: 2,
+        name: "b"
+      }
+      ,
+      {
+        id: 3,
+        name: "c"
+      }
+    ])
+  }
 
+  // bca 
+  // abc
   return (
     <div>
-      {
-        myArr.map((currentElement) => <People page={currentElement} />)
-      }
+      <ul>
+        {/* <li>{data[0]}</li>
+        <li>{data[1]}</li>
+        <li>{data[2]}</li> */}
 
+        {
+          data.map((item) => <li key={item.id}>{item.name}</li>)
+        }
+      </ul>
 
+      <button
+        onClick={handleShuffle}
+      >Shuffle</button>
+
+      <button
+        onClick={handleReset}
+      >
+        Reset
+      </button>
     </div>
   )
 }
